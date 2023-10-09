9b06b3be6dcb3a0eec272ce64477ac18e258bc97
Emiliano BRavo <bravoemiliano74@gmail.com>
2023-10-09 16:37:15 -0300

    configurando la tienda

--- src/app/constants.js
+++ src/app/constants.js
@@ -2,7 +2,7 @@ const phoneNumber = process.env.REACT_APP_PHONE_NUMBER
 const sheet = process.env.REACT_APP_GOOGLE_SHEET_ENDPOINT
 
 export const INFO = {
-  title: "CERDO VA - Lista de precios por mayor",
+  title: "DRINKS - Delivery de bebidas en Córdoba",
   phone: phoneNumber,
   social: {
     instagram: "instagram.com/",
